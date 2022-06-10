6f9196564ff352e948f715db2ec7f3e133daf8b0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-06-11 00:14:10 +0300

    chapter2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,18 @@
-module Test.MySolutions where
+module Test.MySolutions
+  ( circleArea
+  , diagonal
+  , leftoverCents
+  ) where
 
 import Prelude
+import Data.Int (rem)
+import Math (pi, sqrt)
+
+diagonal ∷ Number → Number → Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea ∷ Number → Number
+circleArea r = pi * r * r
+
+leftoverCents ∷ Int → Int
+leftoverCents n = rem n 100
